b00d606532854b89745b8b97aa0726d14ad52dc8
lnikolov <lnikolov@esalink.com>
2022-11-12 09:52:30 +0200

    initial commit

---
+++ src/main/java/com/latchezar/javaspringassignment/JavaSpringAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.latchezar.javaspringassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/latchezar/javaspringassignment/JavaSpringAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.latchezar.javaspringassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
